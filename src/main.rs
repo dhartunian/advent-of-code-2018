1e9f9071cafac6d67b5d11258c6fe9608de38dcf
David Hartunian <david@positiondev.com>
2018-12-07 14:00:46 -0500

    Day 3 part 1

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,40 @@ use std::collections::HashSet;
 use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
+    let mut file = File::open("inputs/day-3-input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let lines = contents.lines();
+    let mut hashy : HashMap<(u32, u32), u32> = HashMap::new();
+    for line in lines {
+        let words : Vec<&str> = line.split(' ').collect();
+        // ["#2", "@", "518,811:", "15x18"]
+        let start_coords : Vec<u32> = words[2].split_at(words[2].len()-1)
+                                   .0
+                                   .split(',')
+                                   .map(|s| s.parse().unwrap())
+                                   .collect();
+        let size : Vec<u32> = words[3].split('x')
+                                      .map(|s| s.parse().unwrap())
+                                      .collect();
+        for x in start_coords[0]..start_coords[0]+size[0] {
+            for y in start_coords[1]..start_coords[1]+size[1] {
+                hashy.entry((x,y)).and_modify(|x| { *x += 1 }).or_insert(1);
+            }
+        }
+    }
+    let mut counter = 0;
+    for v in hashy.values() {
+        if *v >= 2 {
+            counter += 1;
+        }
+    }
+    println!("{}", counter);
+    Ok(())
+}
+
+fn _day_2_part_2() -> std::io::Result<()> {
     let mut file = File::open("inputs/day-2-input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
